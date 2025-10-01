023e603d5e8a5212e4b99fcdcda9bddbbf4b5c4d
burton-c-jpg <burton.c@wustl.edu>
2025-10-01 15:33:52 -0500

    a

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenRadius(.1);
+		StdDraw.setPenColor(0,255,0);
+		StdDraw.filledRectangle(.5,.5,.8,.8);
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledEllipse(.5,.5,0.3,0.3);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.text(.5, .5, "BBEG");
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,7 +18,41 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		Scanner in = new Scanner(f); // making Scanner with a File
+		String shapetype = in.next();
+		int Red = in.nextInt();
+		int Green = in.nextInt();
+		int Blue = in.nextInt();
+		boolean Filled = in.nextBoolean();
+		double Xmidpoint = in.nextDouble();
+		double Ymidpoint = in.nextDouble();
+		double width = in.nextDouble();
+		double height = in.nextDouble();
+		double[] x = {0.8, 0.6,0.3};
+		double[] y = {0.2, 0.5, 0.2 };
+
+		StdDraw.setPenColor(Red, Green, Blue);
+		StdDraw.setPenRadius(.1);
+		if(shapetype.equals("ellipse")){
+		if (Filled == true) {
+			StdDraw.filledEllipse(Xmidpoint, Ymidpoint, width, height);
 		
+		}else {StdDraw.ellipse(Xmidpoint, Ymidpoint, width, height);
+		}
 	}
+	else if(shapetype.equals("rectangle")){
+		if (Filled == true) {
+			StdDraw.filledRectangle(Xmidpoint, Ymidpoint, width, height);
+		
+		}else {StdDraw.rectangle(Xmidpoint, Ymidpoint, width, height);
+		}
+	}	else if(shapetype.equals("triangle")){
+		if (Filled == true) {
+			StdDraw.filledPolygon(x,y);
+		
+		}else {StdDraw.polygon(x,y);
+		}
+
+	}
+}
 }
